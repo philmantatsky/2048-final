2f43b61495195b5f938ef1e0086df61848c04fd5
Phillip Mantatsky <pmantatsky1@bsd220.org>
2024-04-16 09:31:27 -0500

    Initial Commit

---
+++ 2048 final/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  2048 final
+//
+//  Created by Phillip Mantatsky on 4/16/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ 2048 final/_048_finalApp.swift
@@ -0,0 +1,17 @@
+//
+//  _048_finalApp.swift
+//  2048 final
+//
+//  Created by Phillip Mantatsky on 4/16/24.
+//
+
+import SwiftUI
+
+@main
+struct _048_finalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
