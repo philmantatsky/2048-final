8f05a47c331c0d977104f881b14d545a95a193d3
Phillip Mantatsky <pmantatsky1@bsd220.org>
2024-04-17 10:05:59 -0500

    new view commit

--- 2048 final/ContentView.swift
+++ 2048 final/ContentView.swift
@@ -8,14 +8,24 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var moves = Array(repeating: "", count: 9)
-    @State private var board = Array(repeating: "", count: 16)
+    @State private var gameBoard = Array(repeating: [0,0,0,0], count: 4)
     var body: some View {
-        VStack {
-            
+            NavigationView {
+                VStack {
+                    Text("")
+                        .padding()
+
+                    // Navigation link to the game view
+                    NavigationLink(destination: GameView(phrase: "This is from the first view")) {
+                        Text("Play Game")
+                            .bold()
+                            .font(.title)
+                    }
                 }
+                .navigationTitle("4096 The Game")
             }
         }
+    }
 
     #Preview {
         ContentView()
--- 2048 final/gameView.swift
+++ 2048 final/gameView.swift
@@ -7,12 +7,15 @@
 
 import SwiftUI
 
-struct gameView: View {
+struct GameView: View {
+    let phrase: String
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text(phrase)
+            .navigationTitle("Game View")
     }
 }
 
 #Preview {
-    gameView()
+    GameView(phrase: "")
 }
