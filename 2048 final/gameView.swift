46ddf5db1880de8024d0cc5ba670b46f09b600d4
Phillip Mantatsky <pmantatsky1@bsd220.org>
2024-04-17 09:58:58 -0500

    added second view and menu

---
+++ 2048 final/gameView.swift
@@ -0,0 +1,18 @@
+//
+//  gameView.swift
+//  2048 final
+//
+//  Created by Phillip Mantatsky on 4/17/24.
+//
+
+import SwiftUI
+
+struct gameView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    gameView()
+}
