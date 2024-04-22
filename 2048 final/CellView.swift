02f1794e5e75a47a926e7146e84c0202b751b336
Phillip Mantatsky <pmantatsky1@bsd220.org>
2024-04-22 10:09:17 -0500

    CellView added

---
+++ 2048 final/CellView.swift
@@ -0,0 +1,18 @@
+//
+//  CellView.swift
+//  2048 final
+//
+//  Created by Phillip Mantatsky on 4/22/24.
+//
+
+import SwiftUI
+
+struct CellView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CellView()
+}
